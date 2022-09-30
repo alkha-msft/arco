bbbd168c70233ed08e53ed8142d6f38246499e89
Daniel Lopez <llopezmejia@microsoft.com>
2022-09-30 14:33:44 -0700

    Added Voice Recognition feature to focus

--- src/Arco.Client/Assets/Scripts/WorldService.cs
+++ src/Arco.Client/Assets/Scripts/WorldService.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class WorldService : MonoBehaviour
 {
@@ -13,10 +15,26 @@ public class WorldService : MonoBehaviour
     [SerializeField]
     private int _distanceFromUser = 1;
 
+    private KeywordRecognizer _keywordRecognizer;
+
+    private Dictionary<string, System.Action> _keywords;
+
     // Start is called before the first frame update
     void Start()
     {
         UpdatePosition();
+        _keywords = new Dictionary<string, System.Action>() {
+            {"focus", UpdatePosition}
+        };
+        _keywordRecognizer = new KeywordRecognizer(_keywords.Keys.ToArray());
+        _keywordRecognizer.OnPhraseRecognized += OnPhraseRecognized;
+        _keywordRecognizer.Start();
+    }
+
+    void OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        if (_keywords.TryGetValue(args.text, out var action)) {
+            action.Invoke();
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +48,7 @@ public class WorldService : MonoBehaviour
 
     void UpdatePosition()
     {
+        Debug.LogWarning("UpdatePosition");
         var worldContainerPosition = _headPose.position + _headPose.forward * _distanceFromUser;
         transform.position = worldContainerPosition;
     }
